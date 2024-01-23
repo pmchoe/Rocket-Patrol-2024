ce2ed7e9077e28e13f3f5fe458563bbaf7c9357b
pmchoe <pmchoe@ucsc.edu>
2024-01-22 22:40:52 -0800

    finished Rocket Patrol 2024

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -7,6 +7,8 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);   // add to existing, displayList, updateList
         this.isFiring = false;      // track rocket's firing status
         this.moveSpeed = 2;         // rocket speed in pixels/frame
+
+        this.sfxShot = scene.sound.add('sfx-shot');
     }
 
     update() {
@@ -14,14 +16,15 @@ class Rocket extends Phaser.GameObjects.Sprite {
         if(!this.isFiring) {
             if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
                 this.x -= this.moveSpeed;
-            } else if(keyRIGHT.isDown && this.x <= Gamepad.config.width - borderUISize - this.width) {
+            } else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
                 this.x += this.moveSpeed;
             }
         }
 
         // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyFIRE)) {
+        if(Phaser.Input.Keyboard.JustDown(keyFIRE) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxShot.play({volume: 0.4});
         }
         // if fired, move ship up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
@@ -32,5 +35,11 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = false;
             this.y = game.config.height - borderUISize - borderPadding;
         } 
+    }
+
+    // reset rocket to default position
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
